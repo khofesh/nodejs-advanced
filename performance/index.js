f529b6dd412edf3c70c24ee25ebda0733fb38ccf
Fahmi Ahmad <fahmiahmad@tutanota.com>
2020-07-12 09:58:37 +0700

    do pbkdf2

--- performance/index.js
+++ performance/index.js
@@ -1,3 +1,4 @@
+process.env.UV_THREADPOOL_SIZE = 1;
 const cluster = require("cluster");
 
 // is the file being executed in master mode ?
@@ -6,21 +7,16 @@ if (cluster.isMaster) {
   // in child mode
   cluster.fork();
   cluster.fork();
-  cluster.fork();
-  cluster.fork();
 } else {
   // a child
   const express = require("express");
+  const crypto = require("crypto");
   const app = express();
 
-  function doWork(duration) {
-    const start = Date.now();
-    while (Date.now() - start < duration) {}
-  }
-
   app.get("/", (req, res) => {
-    doWork(5000);
-    res.send("hi there");
+    crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+      res.send("hi there");
+    });
   });
 
   app.get("/fast", (req, res) => {
